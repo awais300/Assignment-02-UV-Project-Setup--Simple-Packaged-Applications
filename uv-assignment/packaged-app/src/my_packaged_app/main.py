4d133c5756ae6db0647fa13399ad2240de72783d
Awais <awais300@gmail.com>
2025-08-30 22:07:14 +0500

    Revert "Assignment 02: UV Simple & Packaged Apps"
    This reverts commit 3501bad7992f48dbc0bfa85abbf6d6b73bc81e42.

--- uv-assignment/packaged-app/src/my_packaged_app/main.py
+++
@@ -1,9 +0,0 @@
-def main():
-    print("Muhammad Awais - PIAIC106612")
-    # Example functionality
-    print("Example functionality")
-    for i in range(1, 6):
-        print(f": Square of {i} = {i*i}")
-
-if __name__ == "__main__":
-    main()
--- uv-assignment/simple-app/main.py
+++
@@ -1,7 +0,0 @@
-def main():
-    print("Muhammad Awais - PIAIC106612")
-    # Example functionality
-    print("Examaple functionality: 2 + 3 =", 2 + 3)
-
-if __name__ == "__main__":
-    main()
